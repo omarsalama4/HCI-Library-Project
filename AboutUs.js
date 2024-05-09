e1012354c270d368955c0cb411d72682ab2e0112
Omar Salama <omarsalama117@gmail.com>
2024-05-09 17:49:29 +0300

    few enhancements in home and about us pages

--- AboutUs.js
+++ AboutUs.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("open");
 }
+function navigateToMenuPage() {
+    // Assuming the next page URL is 'nextpage.html'
+    window.location.href = 'About Us.html';
+}
